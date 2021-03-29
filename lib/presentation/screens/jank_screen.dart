a4f5da156193cc06662a4829bbab3798b16709b2
Rashmi Ranganathan <rashmi05rash@gmail.com>
2021-03-29 23:23:58 +0530

    Remove unwanted code

--- lib/presentation/screens/jank_screen.dart
+++ lib/presentation/screens/jank_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:devtools_flutter/common/utils/image_utils.dart';
 import 'package:flutter/material.dart';
 
 class Jank extends StatefulWidget {
@@ -9,38 +8,14 @@ class Jank extends StatefulWidget {
 }
 
 class _JankState extends State<Jank> {
-  int _counter = 20;
-  ScrollController _scrollController;
-
-  void _addMoreImages() {
-    setState(() {
-      _counter += 10;
-    });
-  }
-
-  _scrollListener() {
-    if (_scrollController.offset >=
-        _scrollController.position.maxScrollExtent) {
-      _addMoreImages();
-    }
-  }
-
-  void initState() {
-    _scrollController = ScrollController();
-    _scrollController.addListener(_scrollListener);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
       body: Card(
         color: Colors.amber,
-        margin: EdgeInsets.only(top: 150),
+        margin: EdgeInsets.only(top: 190),
         child: ListView.builder(
-          controller: _scrollController,
-          itemCount: _counter,
           itemBuilder: (context, index) {
             if (index % 50 == 0) {
               return ExpensiveWidget(index);
